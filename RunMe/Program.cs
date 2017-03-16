48a9f3a967bd4b14531b75999d0783a949848c6b
Dmitry Babich <dmitrybabichmail@gmail.com>
2017-03-16 18:23:47 +0300

    Initial

---
+++ RunMe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ultrapico.Expresso;
+
+namespace RunMe
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var mainForm = new MainForm(new string[0]);
+            mainForm.registration.AlreadyRegistered = true;
+            mainForm.registration.UserName = "%Skipped%";
+            Application.Run(mainForm);
+        }
+    }
+}
